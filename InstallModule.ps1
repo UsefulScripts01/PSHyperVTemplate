3b2f1054c0fffbb87574a4a8d5c803831e70d8dc
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2023-03-16 22:01:48 +0100

    comment

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,13 +1,11 @@
-<#
-.SYNOPSIS
-    Install VmModule
-.DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
-#>
-
+# download PSHyperVModule.psm1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UsefulScripts01/PsModules/main/PSHyperVTemplate.psm1" -OutFile "$HOME\Documents\PSHyperVTemplate.psm1"
+
+#  Check if a PowerShell module file exists and create one if necessary
 if (!$PROFILE) { New-Item -Path $PROFILE -Type File -Force }
+
+# Add "Import-Module" command to PS module
 if ($PROFILE) {
-    Add-Content -Value "# Import PowerShell Profile"
+    Add-Content -Value "# Import PSHyperVTemplate.psm1"
     Add-Content -Value "Import-Module -Name $HOME\Documents\PSHyperVTemplate.psm1" -Path $PROFILE -Force
 }
