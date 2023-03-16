dee331d3ed618825f96c99f5f6b400cd93c84ecf
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2023-03-16 13:19:09 +0100

    install module

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -5,8 +5,6 @@
     A longer description of the function, its purpose, common use cases, etc.
 #>
 
-
-# Windows PowerShell
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UsefulScripts01/PsModules/main/VmModule.psm1" -OutFile "$HOME\Documents\VmModule.psm1"
 if (!$PROFILE) { New-Item -Path $PROFILE -Type File -Force }
 if ($PROFILE) { Add-Content -Value "Import-Module -Name $HOME\Documents\VmModule.psm1" -Path $PROFILE -Force }
